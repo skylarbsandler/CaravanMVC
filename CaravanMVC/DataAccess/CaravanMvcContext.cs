74050adc2bbf5ce6ee0f6a34e3b939d21ca03b10
skylarbsandler <skylarbsandler@gmail.com>
2023-08-21 13:57:20 -0600

    Created Database and Seeded Data

--- CaravanMVC/DataAccess/CaravanMvcContext.cs
+++ CaravanMVC/DataAccess/CaravanMvcContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using CaravanMVC.Models;
+using System.Reflection.Emit;
 
 namespace CaravanMVC.DataAccess
 {
@@ -11,6 +12,24 @@ namespace CaravanMVC.DataAccess
         public CaravanMvcContext(DbContextOptions<CaravanMvcContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Wagon>().HasData(
+               new Wagon { Id = 1, Name = "Old Faithful", NumWheels = 4, Covered = false},
+               new Wagon { Id = 2, Name = "Prairie Pioneer", NumWheels = 6, Covered = true },
+               new Wagon { Id = 3, Name = "Westward Wanderer", NumWheels = 8, Covered = true }
+           );
+
+            modelBuilder.Entity<Passenger>().HasData(
+                new Passenger { Id = 1, Name = "Skylar", Age = 26, Destination = "Denver", WagonId = 1 },
+                new Passenger { Id = 2, Name = "Scott", Age = 34, Destination = "Denver", WagonId = 1 },
+                new Passenger { Id = 3, Name = "Kaila", Age = 18, Destination = "San Francisco", WagonId = 2 },
+                new Passenger { Id = 4, Name = "Gabe", Age = 45, Destination = "San Francisco", WagonId = 2 },
+                new Passenger { Id = 5, Name = "Meghan", Age = 60, Destination = "Portland", WagonId = 3 },
+                new Passenger { Id = 6, Name = "Griffin", Age = 39, Destination = "Portland", WagonId = 3 }
+            );
         }
     }
 }
--- CaravanMVC/Models/Wagon.cs
+++ CaravanMVC/Models/Wagon.cs
@@ -2,6 +2,7 @@
 {
     public class Wagon
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public int NumWheels { get; set; }
         public bool Covered { get; set; }
